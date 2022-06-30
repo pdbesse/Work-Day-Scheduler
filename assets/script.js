a53fbd462f0f1e59e442169396c6cbd22af96a28
Phillip <phillip.besse@gmail.com>
2022-06-30 12:49:57 -0400

    current day display working; buttons save entered text to local storage

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var today = moment();
+$("#currentDay").text(today.format("dddd MMMM Do, YYYY"));
+
+// type event into textarea
+// AtC button saves to local storage
+    // need to link text area input to each corresponding button
+
+$("#9btn").on("click", function (){
+    var text9 = $("#9am").children("textarea").val();
+    var time = $("#9am").children(".hour").text()
+
+    localStorage.setItem(time, text9);
+})
+
+$("#10btn").on("click", function (){
+    var text10 = $("#10am").children("textarea").val();
+    var time = $("#10am").children(".hour").text()
+
+    localStorage.setItem(time, text10);
+})
+// repeat for each button?
+// consolidate with event.target?
+
+// have entered text persist in each time block on page reload
+
+// func to turn past events red
+// func to turn current hour different color
+// func to turn future hours different color
+
