2a258f1b6b23722a115bee9b10a3dd026cc7a15d
Phillip <phillip.besse@gmail.com>
2022-06-30 23:33:17 -0400

    trying to get entered items to persist on page reload

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,11 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do, YYYY"));
 
-//
-
 $(".saveBtn").on("click", function() {
     var calEvent = $(this).siblings("textarea").val();
     var calEventTime = $(this).siblings(".hour").text();
     
     localStorage.setItem(calEventTime, calEvent);
-
 })
 
 // have entered text persist in each time block on page reload
@@ -16,14 +13,19 @@ $(".saveBtn").on("click", function() {
 // pseudocode with tutor Joe Young
 // pull out hour from moment
 var realHour = moment().format("H");
+    /*
     console.log(realHour);
+    */
 // pull attribute from HTML, .each
 // for loop to repeat time comparison logic - use "this"
 $(".hour").each(function (){
 //parseInt() look at .split or .splice
+// add MT id to times
     var currentHour = parseInt($(this).attr("id").split("hour-")[1]);
-    /* console.log($(this).attr("id").split("hour-")[1]);
-    console.log(currentHour); */
+    /*
+    console.log($(this).attr("id").split("hour-")[1]);
+    console.log(currentHour); 
+    */
 
 // func to turn past events red
 if (currentHour < realHour) {
@@ -41,8 +43,5 @@ else if (currentHour > realHour) {
 }
 })
 
-
 // local.Storage.getItem
-
-
-// add MT id to times
+$("#hour-9").sibling("textarea").text(localStorage.getItem("9 AM"));
