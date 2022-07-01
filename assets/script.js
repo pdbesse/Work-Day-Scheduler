6955fff8ad86382de166cc5e970765d6119957f7
Phillip <phillip.besse@gmail.com>
2022-06-30 20:19:12 -0400

    .saveBTN saves to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,15 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do, YYYY"));
 
-// type event into textarea
-// AtC button saves to local storage
-    // need to link text area input to each corresponding button
+//
 
-/* $("#9btn").on("click", function (event){
-    var text9 = $("#9am").children("textarea").val();
-    var time = $("#9am").children(".hour").text()
+$(".saveBtn").on("click", function() {
+    var calEvent = $(this).siblings("textarea").val();
+    var calEventTime = $(this).siblings(".hour").text();
+    
+    localStorage.setItem(calEventTime, calEvent);
 
-    localStorage.setItem(time, text9); */
-
-    $(".saveBtn").click(function(event) {
-        var calEvent = $(event.target).sibling("textarea").val;
-        var calEventTime = $(event.target).sibling(".hour").text;
-        
-        localStorage.setItem(calEventTime, calEvent);
-
-    })
-/* }) */
-
-$("#10btn").on("click", function (){
-    var text10 = $("#10am").children("textarea").val();
-    var time = $("#10am").children(".hour").text()
-
-    localStorage.setItem(time, text10);
 })
-// repeat for each button?
-// consolidate with event.target?
 
 // have entered text persist in each time block on page reload
 
@@ -35,3 +17,21 @@ $("#10btn").on("click", function (){
 // func to turn current hour different color
 // func to turn future hours different color
 
+
+// pull attribute from HTML, .each
+$(".hour").each(function (){
+//parseInt() look at .split or .splice
+parseInt()
+// pull out hour from moment
+// for loop to repeat time comparison logic - use "this"
+// var hour < current hour = red
+// func to turn past events red
+// hour = current hour = green
+// func to turn current hour different color
+/// hour > current = blue
+})
+
+
+// local.Storage.getItem
+
+// add MT id to times
